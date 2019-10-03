159d105ee5389d1cb365a914f6342eb6f9d6c46b
Martin <martinhanses@hotmail.com>
2019-10-03 10:37:49 -0400

    Ex3: Final touches
    - Added velocity and acceleration to game over dog
    - Randomized background color each reset
    - Each win now shrinks the images and adds more, making the game more difficult

--- Exercises/exercise3/js/script.js
+++ Exercises/exercise3/js/script.js
@@ -41,10 +41,21 @@ let gameOver = false;
 //Keeps track of whether you can click to reset or not
 let onEndScreen = false;
 
-// Set positions of celestial dog
+// Create positions of animated dog when spawned in
 let godDogY;
-//Set height of celestial beam
+let godDogX;
+//Set height of celestial beam (on which the game over dog is transported)
 let beamHeight = 1000;
+
+//Set speed and velocity variables for animated dog
+let dogSpeed = 1;
+let dogVelocity = 1;
+
+//Create random color values for RGB background
+  let randomColorValue1;
+  let randomColorValue2;
+  let randomColorValue3;
+
 
 
 // preload()
@@ -67,8 +78,7 @@ function preload() {
 
 // setup()
 //
-// Creates the canvas, sets basic modes, draws correct number
-// of decoys in random positions, then the target
+
 function setup() {
   //sets up the playing board
   setUpGame();
@@ -83,34 +93,41 @@ function setup() {
 function draw() {
   if (gameOver) {
 
-    //Create celestial tractor beam
+
+    //Create celestial tractor beam on location of found dog
     fill(255);
-    rect(targetX,targetY-(beamHeight/2),imageSize,beamHeight);
-    ellipse(targetX,targetY,imageSize,imageSize);
-    //Animate dog into the heavens
-    image(targetImage,targetX,godDogY,imageSize,imageSize);
-    godDogY--;
+    rect(targetX, targetY - (beamHeight / 2), imageSize, beamHeight);
+    ellipse(targetX, targetY, imageSize, imageSize);
+    //Draw animated version of target
+    image(targetImage, godDogX, godDogY, imageSize, imageSize);
+    //Animate dog up the screen, into the heavens.
+
+    godDogY -= dogSpeed * (dogVelocity * dogVelocity);
+    dogVelocity += 0.03;
+    //I multiply dogVelocity with itself to create a more curved acceleration,
+    //starting slow and ending very quickly
+
 
 
     // Prepare our typography
     textFont("Helvetica");
 
-    textAlign(CENTER,CENTER);
+    textAlign(CENTER, CENTER);
     noStroke();
     fill(random(255));
 
     // Tell them they won!
     textSize(120);
-    text("DOG HAS ASCENDED",width/2,height/2);
+    text("DOG HAS ASCENDED", width / 2, height / 2);
     textSize(80);
-    text("CLICK TO RELIVE THE HORROR", width/2, height/2+80);
+    text("CLICK TO RELIVE THE HORROR", width / 2, height / 2 + 80);
 
     // Draw a circle around the sausage dog to show where it is (even though
     // they already know because they found it!)
     noFill();
     stroke(random(255));
     strokeWeight(10);
-    ellipse(targetX,targetY,imageSize,imageSize);
+    ellipse(targetX, targetY, imageSize, imageSize);
   }
 }
 
@@ -120,97 +137,113 @@ function draw() {
 function mousePressed() {
 
   //If you click on the game over screen, the game restarts
-  if (onEndScreen === true){
+  if (onEndScreen === true) {
     setUpGame();
-    }
+  }
   // The mouse was clicked!
   // Check if the cursor is in the x range of the target
   // (We're subtracting the image's width/2 because we're using imageMode(CENTER) -
   // the key is we want to determine the left and right edges of the image.)
-  if (mouseX > targetX - targetImage.width/2 && mouseX < targetX + targetImage.width/2) {
+  if (mouseX > targetX - targetImage.width / 2 && mouseX < targetX + targetImage.width / 2) {
     // Check if the cursor is also in the y range of the target
     // i.e. check if it's within the top and bottom of the image
     //Also set target dog's position to celestial dog's starting point
-    //Also increase size of images every reset
-    if (mouseY > targetY - targetImage.height/2 && mouseY < targetY + targetImage.height/2) {
+
+    if (mouseY > targetY - targetImage.height / 2 && mouseY < targetY + targetImage.height / 2) {
       gameOver = true;
+      //Set celestial dog's starting point to found target
       godDogY = targetY;
       onEndScreen = true;
-      imageSize += 10;
+      //Decrease size of images every reset
+      imageSize -= 10;
+      //Increase amount of decoys every reset
+      numDecoys += 30;
     }
   }
 
 
 
 }
-
+// Creates the canvas, sets basic modes, draws correct number
+// of decoys in random positions, then the target
 function setUpGame() {
-    gameOver = false;
-    onEndScreen = false;
+  gameOver = false;
+  onEndScreen = false;
+  //Reset speed and velocity for dog
+  dogSpeed = 1;
+  dogVelocity = 1;
+
+  //Create new random values for RGB background
+  findRandomColor()
+
+  createCanvas(windowWidth, windowHeight);
+  //Use random numbers from findRandomColor to create new background color every reset
+  background(randomColorValue1,randomColorValue2,randomColorValue3);
+  imageMode(CENTER);
+  rectMode(CENTER);
 
 
-    createCanvas(windowWidth,windowHeight);
-    background("#ffff00");
-    imageMode(CENTER);
-    rectMode(CENTER);
-
-    // Use a for loop to draw as many decoys as we need
-    for (let i = 0; i < numDecoys; i++) {
-      // Choose a random location on the canvas for this decoy
-      let x = random(0,width);
-      let y = random(0,height);
-      // Generate a random number we can use for probability
-      let r = random();
-      // Use the random number to display one of the ten decoy
-      // images, each with a 10% chance of being shown
-      // We'll talk more about this nice quality of random soon enough.
-      // But basically each "if" and "else if" has a 10% chance of being true
-      if (r < 0.1) {
-        image(decoyImage1,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.2) {
-        image(decoyImage2,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.3) {
-        image(decoyImage3,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.4) {
-        image(decoyImage4,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.5) {
-        image(decoyImage5,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.6) {
-        image(decoyImage6,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.7) {
-        image(decoyImage7,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.8) {
-        image(decoyImage8,x,y,imageSize,imageSize);
-      }
-      else if (r < 0.9) {
-        image(decoyImage9,x,y,imageSize,imageSize);
-      }
-      else if (r < 1.0) {
-        image(decoyImage10,x,y,imageSize,imageSize);
-      }
+  // Use a for loop to draw as many decoys as we need
+  for (let i = 0; i < numDecoys; i++) {
+    // Choose a random location on the canvas for this decoy
+    let x = random(0, width);
+    let y = random(0, height);
+    // Generate a random number we can use for probability
+    let r = random();
+    // Use the random number to display one of the ten decoy
+    // images, each with a 10% chance of being shown
+    // We'll talk more about this nice quality of random soon enough.
+    // But basically each "if" and "else if" has a 10% chance of being true
+    if (r < 0.1) {
+      image(decoyImage1, x, y, imageSize, imageSize);
+    } else if (r < 0.2) {
+      image(decoyImage2, x, y, imageSize, imageSize);
+    } else if (r < 0.3) {
+      image(decoyImage3, x, y, imageSize, imageSize);
+    } else if (r < 0.4) {
+      image(decoyImage4, x, y, imageSize, imageSize);
+    } else if (r < 0.5) {
+      image(decoyImage5, x, y, imageSize, imageSize);
+    } else if (r < 0.6) {
+      image(decoyImage6, x, y, imageSize, imageSize);
+    } else if (r < 0.7) {
+      image(decoyImage7, x, y, imageSize, imageSize);
+    } else if (r < 0.8) {
+      image(decoyImage8, x, y, imageSize, imageSize);
+    } else if (r < 0.9) {
+      image(decoyImage9, x, y, imageSize, imageSize);
+    } else if (r < 1.0) {
+      image(decoyImage10, x, y, imageSize, imageSize);
     }
-    // Once we've displayed all decoys, we choose a random location for the target
-    targetX = random(0,width);
-    targetY = random(0,height);
+  }
+  // Once we've displayed all decoys, we choose a random location for the target
+  targetX = random(0, width);
+  targetY = random(0, height);
 
-    // And draw it (because it's the last thing drawn, it will always be on top)
-    image(targetImage,targetX,targetY,imageSize, imageSize);
+  // And draw it (because it's the last thing drawn, it will always be on top)
+  image(targetImage, targetX, targetY, imageSize, imageSize);
 
-    //Draw dog on rectangle, with text, in top right CORNER
-    noStroke();
-    fill(227, 160, 5);
-    rect(width-70,80,140,160);
-    fill(0);
-    textStyle(CENTER);
-    textSize(12);
-    text("RELEASE ME FROM \n THIS NIGHTMARE", width - 128,128);
-    image(targetImage, width-64,64);
+  //Draw dog on rectangle, with text, in top right CORNER
+  noStroke();
+  fill(227, 160, 5);
+  rect(width - 70, 80, 140, 160);
+  fill(0);
+  textStyle(CENTER);
+  textSize(12);
+  stroke(50);
+  text("RELEASE ME FROM \n THIS NIGHTMARE", width - 128, 128);
+  image(targetImage, width - 64, 64);
+
+  //set animation starting point for animated dog
+  godDogX = targetX;
+
+}
+
+//This function assigns three random numbers for the RGB values used in the background
+function findRandomColor(){
+  randomColorValue1 = random(0,255);
+  randomColorValue2 = random(0,255);
+  randomColorValue3 = random(0,255);
+
 
 }
