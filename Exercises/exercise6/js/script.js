b268326ab6509675175a1b4006c7211c072ac934
Martin <martinhanses@hotmail.com>
2019-10-24 15:47:21 -0400

    Ex6: Script error 4
    - zebra variable was missing the second parameter

--- Exercises/exercise6/js/script.js
+++ Exercises/exercise6/js/script.js
@@ -30,6 +30,7 @@ function setup() {
   //////////////// FIXED = "tiger" variable was missing the second parameter
   tiger = new Predator(100, 100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
+  //////////////// FIXED = zebra didn't have a parameter for "y" (second parameter nonexistent)
   zebra = new Prey(100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
