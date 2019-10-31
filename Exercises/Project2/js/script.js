09f0bc8696b61dfdc5ccbd65b95b0d1093b17392
Martin <martinhanses@hotmail.com>
2019-10-31 13:26:37 -0400

    P2: Major changes
    - Removed most of previous functions and code
    - Wrote new handleInput for Predator
    - Added drawGrid(), which creates a new playing board that scales with window size

--- Exercises/Project2/js/script.js
+++ Exercises/Project2/js/script.js
@@ -1,54 +1,44 @@
-// Predator-Prey Simulation
-// by Pippin Barr
-//
-// Creates a predator and three prey (of different sizes and speeds)
-// The predator chases the prey using the arrow keys and consumes them.
-// The predator loses health over time, so must keep eating to survive.
 
-// Our predator
-let tiger;
 
-// The three prey
-let antelope;
-let zebra;
-let bee;
 
-// setup()
-//
-// Sets up a canvas
-// Creates objects for the predator and three prey
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
-  antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
-  zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
-  bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
+  createCanvas(windowWidth, windowWidth*0.4);
+
+  player = new Predator();
+
+
+
+
 }
 
-// draw()
-//
-// Handles input, movement, eating, and displaying for the system's objects
 function draw() {
-  // Clear the background to black
-  background(0);
+  background(255);
+  drawGrid();
+  player.display();
+  player.handleInput();
 
-  // Handle input for the tiger
-  tiger.handleInput();
 
-  // Move all the "animals"
-  tiger.move();
-  antelope.move();
-  zebra.move();
-  bee.move();
 
-  // Handle the tiger eating any of the prey
-  tiger.handleEating(antelope);
-  tiger.handleEating(zebra);
-  tiger.handleEating(bee);
+}
 
-  // Display all the "animals"
-  tiger.display();
-  antelope.display();
-  zebra.display();
-  bee.display();
+//draw the playing grid
+function drawGrid() {
+    let tileSize = windowWidth/20;
+
+  // set grid dimensions based on tilesize
+  let gridWidth = tileSize;
+  let gridHeight = tileSize;
+// Draw vertical lines
+  for (let i = 0; gridWidth < width; i++) {
+    line(gridWidth, 0, gridWidth, height);
+    gridWidth += tileSize;
+  }
+  // draw horizontal lines
+  for (let i = 0; gridHeight < height; i++) {
+    line(0, gridHeight, width, gridHeight);
+    gridHeight += tileSize;
+  }
+
+
+
 }
